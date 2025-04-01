6d4bb820329ebb66224432c4cea7c97d34ce83f2
Verga <168764811+WiktorVerga@users.noreply.github.com>
2025-04-01 13:39:56 +0200

    01/04/25
    Non abbiamo ancora fatto nulla, si può cancellare tutto

--- sn4m/src/App.js
+++ sn4m/src/App.js
@@ -1,10 +1,35 @@
+import {useState} from "react";
+import Scheda from "./Components/scheda";
+
 
 function App() {
-  return (
-    <div className="App">
+    const [beers, setBeers] = useState([]);
 
-    </div>
-  );
+    const handleClick = async () => {
+        try {
+            const resp = await fetch('https://api.sampleapis.com/beers/ale');
+            const json = await resp.json();
+            setBeers(json);
+            console.log(beers);
+        } catch (err) {
+            setBeers(err.message);
+        }
+
+    }
+
+    return (
+        <div className="App">
+            <h1 className={"btn"}>fdijsn</h1>
+            <button onClick={() => handleClick()}>
+                cliccami
+            </button>
+
+            {beers.length > 0 && beers.map(beer => (
+                <Scheda key={beer.id} beer={beer} />
+            ))
+            }
+        </div>
+    );
 }
 
 export default App;
